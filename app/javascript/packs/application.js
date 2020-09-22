c6bfdaf7c2db1ad3a70d1c0a0d4d591ec92fb326
Justin Coyne <jcoyne@justincoyne.com>
2020-09-22 14:14:43 -0500

    Upload files to activestorage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import 'controllers'
 
 // import 'bootstrap'
 require.context('../images', true)
 import 'stylesheets/main'
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
