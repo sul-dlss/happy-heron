dbc65657437e417fdb013679e9c7e6c9cdfd9c54
Michael J. Giarlo <mjgiarlo@stanford.edu>
2020-10-06 11:45:55 -0700

    Parallelize CI tests and fix bug in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-RoleTerm.create({ label: 'Author' }, { label: 'Contributor' })
+RoleTerm.create([{ label: 'Author' }, { label: 'Contributor' }])
