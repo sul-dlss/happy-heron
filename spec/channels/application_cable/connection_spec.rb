6cdbd9eb1169403ef5ff558d27a9af273d773d05
Michael J. Giarlo <mjgiarlo@stanford.edu>
2020-10-13 11:23:45 -0700

    Dump test directory and move AC::Connection spec to spec directory

---
+++ spec/channels/application_cable/connection_spec.rb
@@ -0,0 +1,12 @@
+# typed: strict
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ApplicationCable::Connection do
+  it 'has some tests' do
+    # cookies.signed[:user_id] = 42
+    # connect
+    # expect(connection.user_id).to eq('42')
+  end
+end
