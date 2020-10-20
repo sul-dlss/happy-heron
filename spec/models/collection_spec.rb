9d7d13b49c6dd8a65ac229e4a36b7ed735f2ddee
Lara Aasem <lara-aasem@hotmail.com>
2020-10-21 00:19:59 +0200

    Add test case for validating collection contact email

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -11,4 +11,9 @@ RSpec.describe Collection do
   it 'has many works' do
     expect(collection.works).to all(be_a(Work))
   end
+
+  it 'has a valid contact email' do
+    collection.contact_email = 'notavalidemail'
+    expect {collection.save!}.to raise_error(an_instance_of(ActiveRecord::RecordInvalid).and having_attributes(message: "Validation failed: Contact email is invalid"))
+  end
 end
