ca7181c84546701ea9d437ead1e667ce30d18fab
Justin Littman <justinlittman@stanford.edu>
2020-10-21 14:44:44 -0400

    Splits out checking deposit status into separate job.
    closes #202

---
+++ app/jobs/deposit_status_job.rb
@@ -0,0 +1,46 @@
+# typed: false
+# frozen_string_literal: true
+
+# Wait for a deposit into SDR API.
+class DepositStatusJob < BaseDepositJob
+  queue_as :default
+
+  sig { params(work: Work, job_id: Integer).void }
+  def perform(work:, job_id:)
+    result = status(job_id: job_id)
+    # This will force a recheck of status.
+    raise "No result yet for job #{job_id}" if result.nil?
+
+    if result.success?
+      work.druid = result.value!
+      work.deposit!
+    else
+      Honeybadger.notify("Job #{job_id} for work #{work.id} failed with: #{result.failure}")
+    end
+  end
+
+  private
+
+  sig { params(job_id: Integer).returns(T.nilable(Dry::Monads::Result)) }
+  def status(job_id:)
+    login_result = login
+    return login_result unless login_result.success?
+
+    result = SdrClient::BackgroundJobResults.show(url: Settings.sdr_api.url, job_id: job_id)
+    if result[:status] != 'complete'
+      nil
+    elsif result[:output][:errors].present?
+
+      Dry::Monads::Failure(error_msg_for(result[:output][:errors]))
+    else
+      Dry::Monads::Success(result[:output][:druid])
+    end
+  end
+
+  def error_msg_for(errors)
+    error = errors.first
+    error_msg = error[:title]
+    error_msg += ": #{error[:message]}" if error[:message]
+    error_msg
+  end
+end
