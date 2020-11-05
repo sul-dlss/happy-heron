ac3c69671e9b401d8fedddec636914837ceb24b0
Jeremy Nelson <jermnelson@gmail.com>
2020-11-05 12:17:17 -0700

    Restore multimedia to Software or code per PR review

--- app/models/work_type.rb
+++ app/models/work_type.rb
@@ -137,7 +137,7 @@ class WorkType
       new(id: 'image', label: 'Image', icon: 'images', subtypes: IMAGE_TYPES),
       new(id: 'mixed materials', label: 'Mixed Materials', icon: 'play', subtypes: MIXED_TYPES),
       new(id: 'other', label: 'Other', icon: 'archive', subtypes: []),
-      new(id: 'software, mixed materials', label: 'Software or Code', icon: 'mouse', subtypes: SOFTWARE_TYPES),
+      new(id: 'software, multimedia', label: 'Software or Code', icon: 'mouse', subtypes: SOFTWARE_TYPES),
       new(id: 'sound', label: 'Sound', icon: 'microphone-alt', subtypes: SOUND_TYPES),
       new(id: 'text', label: 'Text', icon: 'book-open', subtypes: TEXT_TYPES),
       new(id: 'video', label: 'Video', icon: 'film', subtypes: VIDEO_TYPES)
