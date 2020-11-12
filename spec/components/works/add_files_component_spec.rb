79774aba09a51346af690c4651f6a8390ee5e24b
Justin Coyne <jcoyne@justincoyne.com>
2020-11-12 10:59:31 -0600

    Add a clickable button for file upload
    Fixes #333

--- spec/components/works/add_files_component_spec.rb
+++ spec/components/works/add_files_component_spec.rb
@@ -5,9 +5,10 @@ require 'rails_helper'
 
 RSpec.describe Works::AddFilesComponent do
   let(:form) { ActionView::Helpers::FormBuilder.new(nil, nil, controller.view_context, {}) }
+  let(:rendered) { render_inline(described_class.new(form: form)) }
 
   it 'renders the component' do
-    expect(render_inline(described_class.new(form: form)).to_html)
-      .to include('Add your files')
+    expect(rendered.to_html).to include('Add your files')
+    expect(rendered.css('button.dz-clickable').to_html).to include('Choose files')
   end
 end
