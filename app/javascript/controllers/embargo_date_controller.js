490879213aa3e4cc9eadf55013c6298fada4fae0
aaron-collier <aaron.collier@stanford.edu>
2020-11-17 15:24:05 -0800

    Add container target

--- app/javascript/controllers/embargo_date_controller.js
+++ app/javascript/controllers/embargo_date_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   // static targets = ["search", "container", "error", "selectedTemplate", "addItem"]
-  static targets = ["error"]
+  static targets = ["error", "container"]
 
   // Triggered when edit-deposit controller sends an error event
   error(e) {
