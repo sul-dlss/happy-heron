b9b3a9d916dc476d812ab00adc9378b26355839a
Justin Coyne <jcoyne@justincoyne.com>
2020-12-02 22:24:04 -0600

    Move state labels to i18n
    This is consistent with how we are handling workflow events

--- app/components/works/state_display_component.rb
+++ app/components/works/state_display_component.rb
@@ -1,21 +1,9 @@
-# typed: strict
+# typed: false
 # frozen_string_literal: true
 
 module Works
   # Displays information about the current state of the deposit
   class StateDisplayComponent < ApplicationComponent
-    STATE_DISPLAY_LABELS = T.let(
-      {
-        'first_draft' => 'Draft - Not deposited',
-        'version_draft' => 'New version draft - Not deposited',
-        'pending_approval' => 'Pending approval - Not deposited',
-        'rejected' => 'Returned',
-        'depositing' => 'Deposit in progress <span class="fas fa-spinner fa-pulse"></span>'.html_safe,
-        'deposited' => 'Deposited'
-      }.freeze,
-      T::Hash[String, String]
-    )
-
     sig { params(work: Work).void }
     def initialize(work:)
       @work = work
@@ -26,7 +14,15 @@ module Works
 
     sig { returns(T.nilable(String)) }
     def call
-      STATE_DISPLAY_LABELS.fetch(work.state)
+      value = I18n.t(work.state, scope: 'work.state')
+      return value unless work.depositing?
+
+      safe_join([value, spinner], ' ')
+    end
+
+    sig { returns(String) }
+    def spinner
+      tag.span class: 'fas fa-spinner fa-pulse'
     end
   end
 end
