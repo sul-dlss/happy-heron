78248e467bc4dd07c599c40bb8c628e0721ce630
Johnathan Martin <john.martin@stanford.edu>
2020-12-04 00:27:47 -0800

    edit_deposit_controller: clear errors on target elements before setting errors

--- app/javascript/controllers/files_controller.js
+++ app/javascript/controllers/files_controller.js
@@ -5,7 +5,12 @@ export default class extends Controller  {
 
   // Triggered when edit-deposit controller sends an error event
   error(e) {
-    this.containerTarget.classList.add('is-invalid')
-    this.errorTarget.innerHTML = e.detail.join(' ')
+    if (e.detail === null || e.detail.length == 0) {
+      this.containerTarget.classList.remove('is-invalid')
+      this.errorTarget.innerHTML = ''
+    } else {
+      this.containerTarget.classList.add('is-invalid')
+      this.errorTarget.innerHTML = e.detail.join(' ')
+    }
   }
 }
