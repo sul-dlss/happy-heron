43e2e06120b605c2c8e1a47b13875b563a939988
Justin Coyne <jcoyne@justincoyne.com>
2020-12-08 13:54:14 -0600

    Refactor the help mailer to send in the background

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -4,14 +4,12 @@
 # The endpoint for the help modal
 class HelpsController < ApplicationController
   def create
-    name = params[:name]
-    email = params[:email]
-    affiliation = params[:affiliation]
-    help_how = params[:help_how]
-    why_contact = params[:why_contact]
-    body = "#{name} #{affiliation}\n#{why_contact}"
-
-    HelpsMailer.new.send_jira(email, help_how, body)
+    HelpsMailer.with(name: params[:name],
+                     email: params[:email],
+                     affiliation: params[:affiliation],
+                     help_how: params[:help_how],
+                     why_contact: params[:why_contact])
+               .jira_email.deliver_later
     render json: { status: 'success' }
   end
 end
