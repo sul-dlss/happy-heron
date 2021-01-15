256ec1af61dad46069a5e9080b5249f61681e1b0
Michael J. Giarlo <mjgiarlo@stanford.edu>
2021-01-15 13:18:08 -0800

    Display currently selected license in work form
    This fixes a bug where the topmost license is always presented as the currently selected one.

--- spec/components/works/license_component_spec.rb
+++ spec/components/works/license_component_spec.rb
@@ -4,10 +4,22 @@
 require 'rails_helper'
 
 RSpec.describe Works::LicenseComponent do
-  let(:form) { instance_double(ActionView::Helpers::FormBuilder, label: nil, select: nil) }
+  let(:form) { ActionView::Helpers::FormBuilder.new(nil, work_form, controller.view_context, {}) }
+  let(:rendered) { render_inline(described_class.new(form: form)) }
+  let(:work) { build(:work) }
+  let(:work_form) { WorkForm.new(work) }
 
-  it 'renders the component' do
-    expect(render_inline(described_class.new(form: form)).to_html)
-      .to include('Select a license')
+  context 'with no license selected' do
+    it 'renders the component' do
+      expect(rendered.to_html).to include('Select a license')
+    end
+  end
+
+  context 'with a license selected' do
+    let(:work) { build(:work, license: 'ODbL-1.0') }
+
+    it 'renders the component with the correct license selected' do
+      expect(rendered.to_html).to include('<option selected value="ODbL-1.0">')
+    end
   end
 end
