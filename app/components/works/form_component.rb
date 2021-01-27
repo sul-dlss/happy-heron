a4f85d735c895f67fba8b820ba1cc4b86fcf3684
Justin Coyne <jcoyne@justincoyne.com>
2021-01-27 12:05:11 -0600

    Loosen type declarations for Works::FormComponent
    I believe this may have started when we switched back to local form.
    Fixes #915

--- app/components/works/form_component.rb
+++ app/components/works/form_component.rb
@@ -4,15 +4,15 @@
 module Works
   # The component that renders the form for editing or creating a work.
   class FormComponent < ApplicationComponent
-    sig { returns(WorkForm) }
+    sig { returns(DraftWorkForm) }
     attr_reader :work_form
 
-    sig { params(work_form: WorkForm).void }
+    sig { params(work_form: DraftWorkForm).void }
     def initialize(work_form:)
       @work_form = work_form
     end
 
-    sig { returns(T.any(WorkForm, T::Array[T.any(Collection, WorkForm)])) }
+    sig { returns(T.any(DraftWorkForm, T::Array[T.any(Collection, DraftWorkForm)])) }
     def url
       persisted? ? work_form : [model.collection, work_form]
     end
