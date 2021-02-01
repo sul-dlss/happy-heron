fc0d504d2aedb0daa9ee24f597d6932fead8af16
Peter Mangiafico <peter@mangiafico.org>
2021-02-01 09:56:02 -0800

    move methods to class

--- app/components/dashboard/in_progress_row_component.rb
+++ app/components/dashboard/in_progress_row_component.rb
@@ -16,6 +16,10 @@ module Dashboard
       Dashboard::CollectionHeaderComponent.new(collection: work.collection).name
     end
 
+    def truncated_collection_name
+      truncate(collection_name, length: 100, separator: ' ')
+    end
+
     def edit_work_link
       title = Works::DetailComponent.new(work: work).title
       link_to truncate(title, length: 100, separator: ' '), edit_work_path(work), title: title
