973ee44631bfdb81aa8d15886532f478b9fd86e3
Justin Coyne <jcoyne@justincoyne.com>
2021-02-01 16:47:01 -0600

    Collection name for progress bar on dashboard links to the collection page

--- app/components/dashboard/in_progress_row_component.rb
+++ app/components/dashboard/in_progress_row_component.rb
@@ -16,13 +16,14 @@ module Dashboard
       Dashboard::CollectionHeaderComponent.new(collection: work.collection).name
     end
 
-    def truncated_collection_name
-      truncate(collection_name, length: 100, separator: ' ')
-    end
-
     def edit_work_link
       title = Works::DetailComponent.new(work: work).title
       link_to truncate(title, length: 100, separator: ' '), edit_work_path(work), title: title
+    end
+
+    def show_collection_link
+      truncated_collection_name = truncate(collection_name, length: 100, separator: ' ')
+      link_to truncated_collection_name, work.collection, title: collection_name
     end
   end
 end
