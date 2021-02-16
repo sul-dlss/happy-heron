be9924ee80f1dc223215f78efc1ac1a794d29e6c
aaron-collier <aaron.collier@stanford.edu>
2021-02-16 15:14:45 -0800

    Display release duration on collectio show page

--- spec/features/single_radio_selection_spec.rb
+++ spec/features/single_radio_selection_spec.rb
@@ -17,37 +17,31 @@ RSpec.describe 'Selecting a radio button causes other radio button inputs to be 
       before { visit edit_collection_path(collection) }
 
       it 'shows only one release option as checked and disables child select elements of other options' do
+        # We need duplicate select lists with the same CSS selector
+        delayed_release = all('#collection_release_duration').first
+        depositor_selects = all('#collection_release_duration').last
+
         expect(find('#collection_release_option_immediate')).not_to be_checked
         expect(find('#collection_release_option_delay')).not_to be_checked
         expect(find('#collection_release_option_depositor-selects')).to be_checked
-
-        delayed_release = all('#collection_release_duration').first
         expect(delayed_release).to be_disabled
-
-        depositor_selects = all('#collection_release_duration').last
         expect(depositor_selects).not_to be_disabled
 
         # Disable "depositor-selects" select when "delay" selected
         choose('Delay release until')
-
         expect(find('#collection_release_option_immediate')).not_to be_checked
         expect(find('#collection_release_option_delay')).to be_checked
         expect(find('#collection_release_option_depositor-selects')).not_to be_checked
-
-        # expect(find('#collection_release_duration')).to be_disabled
+        expect(delayed_release).not_to be_disabled
         expect(depositor_selects).to be_disabled
 
-        expect(delayed_release).not_to be_disabled
-
+        # When depositor_selects was clicked, it should have disabled the delay selection
         # Disable "depositor-selects" and "delay" selects when "immediately" selected
         choose('Immediately')
-
         expect(find('#collection_release_option_immediate')).to be_checked
         expect(find('#collection_release_option_delay')).not_to be_checked
         expect(find('#collection_release_option_depositor-selects')).not_to be_checked
-
         expect(depositor_selects).to be_disabled
-
         expect(delayed_release).to be_disabled
       end
     end
