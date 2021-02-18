93ac87c744ed100031ef3215485457e01a45b019
Justin Coyne <jcoyne@justincoyne.com>
2021-02-18 15:58:04 -0600

    Download each of the files from the show page

---
+++ app/components/works/show/files_component.rb
@@ -0,0 +1,17 @@
+# typed: false
+# frozen_string_literal: true
+
+module Works
+  module Show
+    # Draws the files section of the show page
+    class FilesComponent < ApplicationComponent
+      def initialize(work_version:)
+        @work_version = work_version
+      end
+
+      attr_reader :work_version
+
+      delegate :attached_files, to: :work_version
+    end
+  end
+end
---
+++ spec/components/works/show/files_component_spec.rb
@@ -0,0 +1,19 @@
+# typed: false
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Works::Show::FilesComponent, type: :component do
+  let(:rendered) { render_inline(described_class.new(work_version: work_version)) }
+  let(:work_version) { create(:work_version) }
+
+  context 'with an attached file' do
+    before do
+      create(:attached_file, :with_file, work_version: work_version)
+    end
+
+    it 'shows a download link' do
+      expect(rendered.css('a').last['href']).to start_with '/rails/active_storage/blobs/redirect/'
+    end
+  end
+end
