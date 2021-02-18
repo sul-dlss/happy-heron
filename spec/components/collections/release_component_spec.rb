6d9bbf1120d9716578f5e09d11d4374c5c07b406
Jeremy Nelson <jermnelson@gmail.com>
2021-02-18 10:51:53 -0700

    New tests for release_info strings

--- spec/components/collections/release_component_spec.rb
+++ spec/components/collections/release_component_spec.rb
@@ -7,13 +7,31 @@ RSpec.describe Collections::ReleaseComponent, type: :component do
   let(:rendered) { render_inline(described_class.new(collection: collection)) }
 
   context 'when displaying a collection' do
-    let(:release_option) { collection.release_option }
     let(:access) { collection.access }
     let(:collection) { build_stubbed(:collection) }
 
     it 'renders the release component' do
-      expect(rendered.css('table').to_html).to include(release_option)
+      expect(rendered.css('table').to_html).to include('Immediately')
       expect(rendered.css('table').to_html).to include(access)
+    end
+  end
+
+  context 'when displaying a collection with depositor selects the release' do
+    let(:collection) { build_stubbed(:collection, release_option: 'depositor-selects', release_duration: '3 years') }
+    let(:rendered) { render_inline(described_class.new(collection: collection)) }
+    let(:message) { 'depositor selects release date at no more than 3 years from date of deposit' }
+
+    it 'renders the release component with more detail regarding the release' do
+      expect(rendered.css('table').to_html).to include(message)
+    end
+  end
+
+  context 'when displaying a collection with a delayed release' do
+    let(:collection) { build_stubbed(:collection, release_option: 'delay', release_duration: '2 years') }
+    let(:rendered) { render_inline(described_class.new(collection: collection)) }
+
+    it 'renders the release component with a delayed option' do
+      expect(rendered.css('table').to_html).to include('2 years from date of deposit')
     end
   end
 end
