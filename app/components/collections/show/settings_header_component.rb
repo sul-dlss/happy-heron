6624d45bc1dce4619bc63d3ad7003c63b4fb4a1b
Justin Coyne <jcoyne@justincoyne.com>
2021-03-01 08:34:00 -0600

    Links on the collection details page go to the collection details form

---
+++ app/components/collections/show/settings_header_component.rb
@@ -0,0 +1,39 @@
+# typed: false
+# frozen_string_literal: true
+
+module Collections
+  module Show
+    # Renders the header for the collection show page (title and create new link)
+    class SettingsHeaderComponent < ApplicationComponent
+      sig { params(collection_version: CollectionVersion).void }
+      def initialize(collection_version:)
+        @collection_version = collection_version
+      end
+
+      sig { returns(CollectionVersion) }
+      attr_reader :collection_version
+
+      delegate :depositing?, :collection, to: :collection_version
+
+      sig { returns(String) }
+      def name
+        collection_version.name.presence || 'No Title'
+      end
+
+      sig { returns(String) }
+      def spinner
+        tag.span class: 'fas fa-spinner fa-pulse'
+      end
+
+      def can_create_work?
+        collection_version.accessioned?
+      end
+
+      def edit_link
+        render Collections::EditLinkComponent.new(collection_version: collection_version,
+                                                  anchor: '',
+                                                  label: "Edit #{name}")
+      end
+    end
+  end
+end
---
+++ spec/components/collections/show/settings_header_component_spec.rb
@@ -0,0 +1,25 @@
+# typed: false
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Collections::Show::SettingsHeaderComponent, type: :component do
+  let(:rendered) { render_inline(described_class.new(collection_version: collection_version)) }
+
+  context 'with a new, first draft collection' do
+    let(:collection_version) { build_stubbed(:collection_version, :first_draft) }
+
+    it 'does not render the spinner' do
+      expect(rendered.to_html).not_to include 'fa-spinner'
+    end
+  end
+
+  context 'with a depositing collection' do
+    let(:collection_version) { build_stubbed(:collection_version, :depositing) }
+
+    it 'does not render the spinner' do
+      expect(rendered.to_html).to include 'Depositing'
+      expect(rendered.to_html).to include 'fas fa-spinner fa-pulse'
+    end
+  end
+end
