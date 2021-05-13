0414a72ff98811cd067179f4b8e1d05011627c31
Aaron Collier <aaron.collier@stanford.edu>
2021-05-13 10:56:24 -0700

    Add simple keywords_row test

---
+++ spec/components/works/keywords_row_component_spec.rb
@@ -0,0 +1,23 @@
+# typed: false
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Works::KeywordsRowComponent do
+  subject(:rendered) { render_inline(described_class.new(form: form_builder, key: 'collection.contact_email')) }
+
+  let(:work) { work_version.work }
+  let(:work_version) { build_stubbed(:work_version) }
+  let(:work_form) { WorkForm.new(work_version: work_version, work: work) }
+  let(:keyword_form) do
+    work_form.prepopulate!
+    work_form.keywords.first
+  end
+  let(:form_builder) do
+    ActionView::Helpers::FormBuilder.new('work', keyword_form, controller.view_context, {})
+  end
+
+  it 'renders the component' do
+    expect(rendered.to_html).to include('Keyword')
+  end
+end
