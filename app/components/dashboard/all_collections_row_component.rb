5b70756f7f9bbc54d286dc725906d3a77b523bb1
Aaron Collier <aaron.collier@stanford.edu>
2021-05-14 16:02:24 -0700

    Add draft label to collections list on dashboard

---
+++ app/components/dashboard/all_collections_row_component.rb
@@ -0,0 +1,26 @@
+# typed: true
+# frozen_string_literal: true
+
+module Dashboard
+  # Renders a list of all collections
+  class AllCollectionsRowComponent < ApplicationComponent
+    sig { params(collection: Collection, counts: Hash).void }
+    def initialize(collection:, counts:)
+      @collection = collection
+      @counts = counts
+    end
+
+    attr_reader :collection, :counts
+
+    def draft_label
+      case @collection.head.state
+      when 'first_draft'
+        return '- Draft'
+      when 'version_draft'
+        return '- Version Draft'
+      end
+
+      nil
+    end
+  end
+end
