e6d09d88df787b72511d28b1e46edc2d207f83e8
Justin Coyne <jcoyne@justincoyne.com>
2021-05-17 13:35:14 -0500

    Scroll to first invalid element on the form
    Fixes #1365
    Fixes #1382

--- app/packs/modules/validate-forms.js
+++ app/packs/modules/validate-forms.js
@@ -1,6 +1,7 @@
 // JavaScript to disable form submissions if there are invalid fields
 (function () {
   'use strict'
+
   window.addEventListener('turbo:load', function() {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
@@ -13,10 +14,25 @@
           if (!form.checkValidity()) {
             event.preventDefault()
             event.stopPropagation()
+            scrollToFirstInvalidElement(form)
           }
 
           form.classList.add('was-validated')
         }, false)
       })
   })
+
+  function scrollToFirstInvalidElement(form) {
+    const yOffset = -10
+    const elem = form.querySelector(':invalid')
+    const originalDisplay = elem.style.display
+    // Allow hidden element to scroll into display.
+    elem.style.display = 'block'
+    const y = elem.getBoundingClientRect().top + window.pageYOffset + yOffset
+    elem.focus({preventScroll: true})
+    elem.style.display = originalDisplay
+
+    // elem.focus() and elem.scrollIntoView() doesn't give us any margin, so we do this more complex method:
+    window.scrollTo({top: y, behavior: 'smooth'})
+  }
 })()
