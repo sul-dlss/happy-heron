cc1fd85ddacb378387e81ba4abd6e36c5838fc1c
Justin Coyne <jcoyne@justincoyne.com>
2021-05-17 13:13:35 -0500

    Extract more-types controller from edit-deposit controller
    This keeps the function of each controller clear.

--- app/packs/controllers/edit_deposit_controller.js
+++ app/packs/controllers/edit_deposit_controller.js
@@ -1,9 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["moreTypesLink",
-                    "moreTypes",
-                    "frame"]
+  static targets = ["frame"]
   static values = { endpoint: String }
 
   connect() {
@@ -11,6 +9,7 @@ export default class extends Controller {
     this.element.addEventListener('change', () => this.check())
   }
 
+  // Trigger the progress component to re-render on each change
   check(e) {
     const form = this.element
     const data = new FormData(form)
@@ -24,25 +23,5 @@ export default class extends Controller {
 
     const queryString = new URLSearchParams(data).toString();
     this.frameTarget.src = `${this.endpointValue}?${queryString}`
-  }
-
-  toggleMoreTypes(event) {
-    event.preventDefault()
-
-    this.moreTypesTarget.hidden ?
-      this.showMoreTypes() :
-      this.hideMoreTypes()
-  }
-
-  showMoreTypes() {
-    this.moreTypesTarget.hidden = false
-    this.moreTypesLinkTarget.innerHTML = 'See fewer options'
-    this.moreTypesLinkTarget.classList.toggle('collapsed', false)
-  }
-
-  hideMoreTypes() {
-    this.moreTypesTarget.hidden = true
-    this.moreTypesLinkTarget.innerHTML = 'See more options'
-    this.moreTypesLinkTarget.classList.toggle('collapsed', true)
   }
 }
