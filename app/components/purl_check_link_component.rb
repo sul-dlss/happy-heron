27a0e25be6938171594d3003479ef38b1b34fa74
Jeremy Nelson <jermnelson@gmail.com>
2021-05-18 10:25:52 -0600

    Purl Check Link component that launches the workType modal depending on version state

---
+++ app/components/purl_check_link_component.rb
@@ -0,0 +1,32 @@
+# typed: false
+# frozen_string_literal: true
+
+# Renders a widget for describing a related link.
+class PurlCheckLinkComponent < ApplicationComponent
+
+  def initialize(work_version:, anchor: nil, label: nil, css_class: nil)
+    @work_version = work_version
+    @anchor = anchor
+    @label = label
+    @css_class = css_class
+  end
+
+  def choose_label
+    return "Choose or Edit #{get_title}" if label.nil?
+    label
+  end
+
+  def edit_label
+    return "Edit #{get_title}" if label.nil?
+    label
+  end
+
+  attr_reader :work_version, :anchor, :label, :css_class
+
+  private
+
+  def get_title
+    Works::DetailComponent.new(work_version: work_version).title
+  end
+
+end
