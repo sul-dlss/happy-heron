834efd6065f44faff942c84b320af334464e58ef
Aaron Collier <aaron.collier@stanford.edu>
2021-05-25 14:33:13 -0700

    Use DraftVersionForm propertly in spec

--- spec/components/collections/version_description_component_spec.rb
+++ spec/components/collections/version_description_component_spec.rb
@@ -7,13 +7,21 @@ RSpec.describe Collections::VersionDescriptionComponent do
   let(:form) { ActionView::Helpers::FormBuilder.new(nil, collection_form, controller.view_context, {}) }
   let(:rendered) { render_inline(described_class.new(form: form)) }
   let(:collection) { build_stubbed(:collection, head: collection_version) }
-  let(:collection_form) { CreateCollectionForm.new(collection: collection, collection_version: collection_version) }
+  let(:collection_form) { DraftCollectionVersionForm.new(collection_version) }
 
   context 'with a first draft' do
     let(:collection_version) { build_stubbed(:collection_version) }
 
     it 'does not renders the component' do
       expect(rendered.to_html).not_to include('Version your work')
+    end
+  end
+
+  context 'with a deposited collection' do
+    let(:collection_version) { build_stubbed(:collection_version, state: :deposited) }
+
+    it 'renders the component' do
+      expect(rendered.to_html).to include('Version your work')
     end
   end
 end
