18b048b2aae3fcb7893540599dbde28beeae6440
Aaron Collier <aaron.collier@stanford.edu>
2021-05-27 14:05:25 -0700

    Make the datatables controller configurable

--- app/packs/controllers/datatables_controller.js
+++ app/packs/controllers/datatables_controller.js
@@ -2,11 +2,22 @@ import { Controller } from "stimulus"
 import { DataTable } from "simple-datatables"
 
 export default class extends Controller {
+  static values = { selector: String }
   connect() {
-    new DataTable("table", {
-      columns: [
-        { select: 4, sort: "desc" },  // Sort the fifth column in ascending order
-        { select: [1, 5, 6, 7, 8], sortable: false } // Disable unsortable columns
-      ]})
+    if (this.selectorValue == 'works') {
+      new DataTable("#worksTable", {
+        columns: [
+          { select: 4, sort: "desc" },  // Sort the fifth column in ascending order
+          { select: [1, 5, 6, 7, 8], sortable: false } // Disable unsortable columns
+        ]})
+    }
+
+    if (this.selectorValue == 'collections') {
+      new DataTable("#collectionsTable", {
+        columns: [
+          { select: 4, sort: "desc" },  // Sort the fifth column in ascending order
+          { select: [1, 5, 6, 7, 8], sortable: false } // Disable unsortable columns
+        ]})
+    }
   }
 }
