f943d46cf90eec0134387bbdd3ffb1b46c282201
Aaron Collier <aaron.collier@stanford.edu>
2021-05-27 15:00:19 -0700

    Disable search and paging for the all collections table

--- app/packs/controllers/datatables_controller.js
+++ app/packs/controllers/datatables_controller.js
@@ -3,21 +3,23 @@ import { DataTable } from "simple-datatables"
 
 export default class extends Controller {
   static values = { selector: String }
+
   connect() {
     if (this.selectorValue == 'works') {
       new DataTable("#worksTable", {
         columns: [
-          { select: 4, sort: "desc" },  // Sort the fifth column in ascending order
+          { select: 4, sort: "desc" },  // Sort the fifth column in descending order
           { select: [1, 5, 6, 7, 8], sortable: false } // Disable unsortable columns
         ]})
     }
 
     if (this.selectorValue == 'collections') {
       new DataTable("#collectionsTable", {
+        paging: false,
         columns: [
-          { select: 4, sort: "desc" },  // Sort the fifth column in ascending order
-          { select: [1, 5, 6, 7, 8], sortable: false } // Disable unsortable columns
-        ]})
+          { select: 0, sort: "asc" },  // Sort the first column in ascending order
+        ]
+      })
     }
   }
 }
