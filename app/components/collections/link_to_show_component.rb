18794116bc8bc61e6f2fc42e891eb6871e1036cb
Naomi Dushay <ndushay@stanford.edu>
2021-06-04 11:03:32 -0700

    approvals table: link collection names to collection view page

---
+++ app/components/collections/link_to_show_component.rb
@@ -0,0 +1,23 @@
+# typed: false
+# frozen_string_literal: true
+
+module Collections
+  # Draws the link to the collection show page
+  class LinkToShowComponent < ApplicationComponent
+    def initialize(collection_version:)
+      @collection_version = collection_version
+    end
+
+    def link
+      link_to name, collection, name: name
+    end
+
+    def name
+      @name ||= Collections::DetailComponent.new(collection_version: collection_version).name
+    end
+
+    attr_reader :collection_version
+
+    delegate :collection, to: :collection_version
+  end
+end
