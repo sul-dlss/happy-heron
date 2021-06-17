0ee729f47dd57b52a2d9655d56e6b29ec9db31b4
Justin Littman <justinlittman@stanford.edu>
2021-06-17 17:00:45 -0400

    Makes title and related works into text areas.
    closes #1032

---
+++ app/packs/controllers/no_newlines_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "stimulus"
+
+// This controller removes newlines from an input such as a textarea.
+// This allows the input to wrap long text.
+export default class extends Controller {
+  static targets = ["input"]
+
+  change() {
+    this.inputTarget.value = this.inputTarget.value.replace( /\r?\n/gi, '' )
+  }
+}
