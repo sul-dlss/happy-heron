70efe9abd4eb1a21e88726b20d34822312554e1c
Justin Littman <justinlittman@stanford.edu>
2021-06-18 12:28:33 -0400

    Adds version comment for collections.
    closes #789

--- spec/components/collections/information_component_spec.rb
+++ spec/components/collections/information_component_spec.rb
@@ -6,12 +6,20 @@ require 'rails_helper'
 RSpec.describe Collections::InformationComponent, type: :component do
   let(:rendered) { render_inline(described_class.new(collection_version: collection_version)) }
 
-  context 'when displaying a collection' do
+  context 'when a first version collection' do
     let(:collection_version) { build_stubbed(:collection_version) }
 
     it 'renders the information component' do
-      expect(rendered.css('table').to_html).to include collection_version.version.to_s
+      expect(rendered.css('table').to_html).to include '1 - initial version'
       expect(rendered.css('table').to_html).to include collection_version.collection.creator.sunetid
+    end
+  end
+
+  context 'when a subsequent version collection' do
+    let(:collection_version) { build_stubbed(:collection_version, version: 2, description: 'changed the title') }
+
+    it 'renders the information component' do
+      expect(rendered.css('table').to_html).to include '2 - changed the title'
     end
   end
 end
