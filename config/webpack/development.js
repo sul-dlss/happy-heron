5887c530a7fa6676f03042373df12dc919d94e87
Justin Coyne <jcoyne@justincoyne.com>
2021-07-07 11:57:28 -0500

    Upgrade webpack-dev-server to 4.0.beta
    This removes a CVE in glob-parent: https://github.com/sul-dlss/happy-heron/security/dependabot/yarn.lock/glob-parent/open

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,39 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const webpackConfig = require('./base')
 
+
+const devServerConfig = {
+  client: {
+    logging: webpackConfig.devServer.clientLogLevel,
+    overlay: webpackConfig.devServer.overlay,
+    needClientEntry: webpackConfig.devServer.injectClient,
+    needHotEntry: webpackConfig.devServer.injectHot,
+  },
+  compress: webpackConfig.devServer.compress,
+  devMiddleware: {
+    publicPath: webpackConfig.devServer.publicPath,
+  },
+  firewall: !webpackConfig.devServer.disableHostCheck,
+  host: webpackConfig.devServer.host,
+  port: webpackConfig.devServer.port,
+  https: webpackConfig.devServer.https,
+  hot: webpackConfig.devServer.hot,
+  historyApiFallback: webpackConfig.devServer.historyApiFallback,
+  headers: webpackConfig.devServer.headers,
+  static: [
+    {
+      directory: webpackConfig.devServer.contentBase,
+      watch: true,
+    }
+  ],
+};
+if (webpackConfig.devServer.quiet) {
+  webpackConfig.infrastructureLogging.level = 'none';
+}
+if (webpackConfig.devServer.useLocalIp) {
+  devServerConfig.host = 'local-ip';
+}
+webpackConfig.stats = webpackConfig.devServer.stats;
+webpackConfig.devServer = devServerConfig;
+
 module.exports = webpackConfig
