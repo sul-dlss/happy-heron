c05bb6eaf8fa5c016f5874740d12f080dd8e7c80
Justin Littman <justinlittman@stanford.edu>
2021-07-08 11:55:36 -0400

    Switches autocomplete to use OCLC Fast endpoint.
    closes #1755

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -3,55 +3,103 @@
 
 # A proxy to Settings.autocomplete_lookup.url to get OCLC's FAST data for typeahead
 class AutocompleteController < ApplicationController
+  T.unsafe(self).include Dry::Monads[:result]
+
   def show
-    query = params.require(:q)
-    lookup_resp = lookup(query)
-    if lookup_resp.success? || lookup_resp.status == :no_content
-      @suggestions = suggestions(lookup_resp.body)
+    result = lookups(params.require(:q))
+    @suggestions = result.value_or([])
+    if result.success?
       render status: :no_content, html: '' and return if @suggestions.empty?
 
       render status: :ok, layout: false
     else
-      logger.warn("Autocomplete results for #{query} returned #{lookup_resp.status}")
-      @suggestions = []
+      logger.warn(result.failure)
       render status: :internal_server_error, html: ''
     end
   end
 
   private
 
-  # rubocop:disable Metrics/AbcSize
-  def lookup(query)
-    lookup_connection.get do |req|
-      req.params['query'] = query
-      req.params['queryIndex'] = Settings.autocomplete_lookup.query_index
-      req.params['queryReturn'] = URI::DEFAULT_PARSER.escape(Settings.autocomplete_lookup.query_return)
-      req.params['rows'] = Settings.autocomplete_lookup.num_records
-      req.params['suggest'] = 'autoSubject'
+  def lookups(query)
+    # Try first without wildcard.
+    result = lookup(query)
+    return result if result.failure?
+
+    suggestions = result.value!
+    if suggestions.size < num_records
+      # If not enough results then add wildcard.
+      wildcard_result = lookup(query, wildcard: true)
+      wildcard_suggestions = wildcard_result.value_or([])
     end
+
+    Success(merge_suggestions(suggestions, wildcard_suggestions, query))
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  def lookup(query, wildcard: false)
+    resp = lookup_connection.get do |req|
+      req.params['q'] = wildcard ? "keywords:(#{query}*)" : "keywords:(#{query})"
+      # Requesting extra records to try to increase the likelihood that get left anchored matches
+      # since results are sorted by usage.
+      req.params['rows'] = num_records * 2
+      req.params['start'] = 0
+      req.params['version'] = '2.2'
+      req.params['indent'] = 'on'
+      req.params['fl'] = 'id,fullphrase'
+      req.params['sort'] = 'usage desc'
+    end
+
+    return Failure("Autocomplete results for #{query} returned #{resp.status}") unless resp.success?
+
+    Success(parse(resp.body))
   end
   # rubocop:enable Metrics/AbcSize
+
+  def parse(body)
+    ng = Nokogiri::XML(body)
+    ng.root.xpath('/response/result/doc').map do |doc_node|
+      id = doc_node.at_xpath('str[@name="id"]').content
+      label = doc_node.at_xpath('str[@name="fullphrase"]').content
+      { label => authority_uri(id) }
+    end
+  end
 
   def lookup_connection
     @lookup_connection ||= Faraday.new(
       url: Settings.autocomplete_lookup.url,
       headers: {
-        'Accept' => 'application/json',
+        'Accept' => 'application/xml',
         'User-Agent' => 'Stanford Self-Deposit (Happy Heron)'
       }
     )
   end
 
-  # returns the first ten deduplicated _authorized_ forms of FAST terms that match the entered letters.
-  # transform the response to return an array of the first 10 unique hashes
-  #   key: the authorized form of suggestion and
-  #   value: the uri for the OCLC FAST record.
-  def suggestions(lookup_response)
-    return [] if lookup_response.blank?
+  def num_records
+    Settings.autocomplete_lookup.num_records
+  end
 
-    parsed = JSON.parse(lookup_response)
-    result = parsed['response']['docs'].map { |doc| { doc['auth'] => authority_uri(doc['idroot']) } }.flatten
-    result.uniq.first(10)
+  # Split into left-anchored matches to query and other matches.
+  def partition_suggestions(suggestions, query)
+    Array(suggestions).partition { |suggestion| suggestion.keys.first.downcase.start_with?(query.downcase) }
+  end
+
+  def merge_suggestions(suggestions1, suggestions2, query)
+    left_match_suggestions1, other_suggestions1 = partition_suggestions(suggestions1, query)
+    left_match_suggestions2, other_suggestions2 = partition_suggestions(suggestions2, query)
+
+    # Order is left match (non-wildcard) alpha, left match (wildcard) alpha, other matches alpha
+    left_match_suggestions = (sort_suggestions(left_match_suggestions1) + sort_suggestions(left_match_suggestions2))
+                             .take(num_records)
+    num_other_suggestions = num_records - left_match_suggestions.size
+    other_suggestions = (other_suggestions1 + other_suggestions2)
+                        .uniq
+                        .take(num_other_suggestions)
+
+    left_match_suggestions + sort_suggestions(other_suggestions)
+  end
+
+  def sort_suggestions(suggestions)
+    suggestions.sort { |suggestion1, suggestion2| suggestion1.keys.first <=> suggestion2.keys.first }
   end
 
   URI_PREFIX = 'http://id.worldcat.org/fast/'
