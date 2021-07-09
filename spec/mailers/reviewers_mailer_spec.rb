4f9e11f875dab3f72cd2f123c8565980c6167403
Justin Coyne <jcoyne@justincoyne.com>
2021-07-09 12:29:22 -0500

    Add a direct link to the work under review in the email
    Fixes #1721

--- spec/mailers/reviewers_mailer_spec.rb
+++ spec/mailers/reviewers_mailer_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe ReviewersMailer, type: :mailer do
   describe 'submitted_email' do
     let(:user) { build(:user, name: 'Al Dente') }
     let(:mail) { described_class.with(user: user, work_version: work_version).submitted_email }
-    let(:work) { build(:work, collection: collection, depositor: user) }
-    let(:work_version) { build_stubbed(:work_version, :pending_approval, work: work) }
+    let(:work) { build_stubbed(:work, collection: collection, depositor: user) }
+    let(:work_version) { build_stubbed(:work_version, :pending_approval, work: work, title: 'Test title') }
     let(:collection) { build(:collection, :with_reviewers, head: collection_version) }
     let(:collection_version) { build(:collection_version, name: 'small batch organic') }
 
@@ -19,8 +19,8 @@ RSpec.describe ReviewersMailer, type: :mailer do
     end
 
     it 'renders the reason' do
-      expect(mail.body.encoded).to match 'The Depositor Al Dente has submitted a ' \
-                                         'deposit for review in the small batch organic collection.'
+      expect(mail.body.encoded).to match "The Depositor Al Dente has submitted the deposit \"Test title\" for\r\n" \
+                                         '  review in the small batch organic collection.'
     end
   end
 end
