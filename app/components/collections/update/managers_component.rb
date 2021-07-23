d26c886af4daa5685af1c4e04763b1a2b6046d40
Justin Coyne <jcoyne@justincoyne.com>
2021-07-23 13:49:39 -0500

    Extract components for the form contributors

---
+++ app/components/collections/update/managers_component.rb
@@ -0,0 +1,15 @@
+# typed: true
+# frozen_string_literal: true
+
+module Collections
+  module Update
+    # Draws the widget for adding managers to the collection
+    class ManagersComponent < ApplicationComponent
+      def initialize(form:)
+        @form = form
+      end
+
+      attr_reader :form
+    end
+  end
+end
---
+++ spec/components/collections/update/managers_component_spec.rb
@@ -0,0 +1,16 @@
+# typed: false
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Collections::Update::ManagersComponent, type: :component do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  # it "renders something useful" do
+  #   expect(
+  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
+  #   ).to include(
+  #     "Hello, components!"
+  #   )
+  # end
+end
