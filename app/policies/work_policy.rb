478e855c5a0e951ce5eb29bd20deb1ca0cb63d62
Peter Mangiafico <peter@mangiafico.org>
2021-08-12 14:12:45 -0700

    update policy to allow for rejected or pending approval first drafts to be deleted

--- app/policies/work_policy.rb
+++ app/policies/work_policy.rb
@@ -12,10 +12,20 @@ class WorkPolicy < ApplicationPolicy
   end
 
   def destroy?
-    (administrator? || depositor?) && record.persisted? && (record.head.first_draft? || record.head.purl_reservation?)
+    (administrator? || depositor?) && record.persisted? && (first_draft? || first_draft_in_review?)
   end
 
   delegate :administrator?, to: :user_with_groups
+
+  # a first draft of a deposit or a reserved purl
+  def first_draft?
+    record.head.first_draft? || record.head.purl_reservation?
+  end
+
+  # a first draft of a deposit in reviewer workflow that is either pending approval or returned
+  def first_draft_in_review?
+    record.head.version == 1 && (record.head.pending_approval? || record.head.rejected?)
+  end
 
   def depositor?
     record.depositor == user
