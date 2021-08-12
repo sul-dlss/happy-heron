1d393f110cebdfbd5b4a1c756a0a56a1cbe0eafc
Justin Coyne <jcoyne@justincoyne.com>
2021-08-12 11:48:17 -0500

    Mail the doi link in the notifications
    Fixes #1936

--- spec/mailers/works_mailer_spec.rb
+++ spec/mailers/works_mailer_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe WorksMailer, type: :mailer do
   describe 'deposited_email' do
     let(:user) { work.depositor }
     let(:mail) { described_class.with(user: user, work_version: work_version).deposited_email }
-    let(:work) { build_stubbed(:work, collection: collection, druid: 'druid:bc123df4567') }
+    let(:work) { build_stubbed(:work, collection: collection, druid: 'druid:bc123df4567', doi: '10.001/bc123df4567') }
     let(:work_version) { build_stubbed(:work_version, :deposited, title: 'Photo booth activated charcoal', work: work) }
     let(:collection) { build_stubbed(:collection, head: collection_version) }
     let(:collection_version) { build_stubbed(:collection_version, name: 'gastropub humblebrag taiyaki') }
@@ -42,13 +42,14 @@ RSpec.describe WorksMailer, type: :mailer do
     it 'renders the body' do
       expect(mail.body.encoded).to have_content('“Photo booth activated charcoal”')
       expect(mail.body.encoded).to have_content('gastropub humblebrag taiyaki collection')
+      expect(mail.body.encoded).to have_content('https://doi.org/10.001/bc123df4567')
     end
   end
 
   describe 'new_version_deposited_email' do
     let(:user) { work.depositor }
     let(:mail) { described_class.with(user: user, work_version: work_version).new_version_deposited_email }
-    let(:work) { build_stubbed(:work, collection: collection, druid: 'druid:bc123df4567') }
+    let(:work) { build_stubbed(:work, collection: collection, druid: 'druid:bc123df4567', doi: '10.001/bc123df4567') }
     let(:work_version) { build_stubbed(:work_version, :deposited, title: 'twee retro man braid', work: work) }
     let(:collection) { build_stubbed(:collection, head: collection_version) }
     let(:collection_version) { build_stubbed(:collection_version, name: 'listicle fam ramps flannel') }
@@ -62,13 +63,14 @@ RSpec.describe WorksMailer, type: :mailer do
     it 'renders the body' do
       expect(mail.body.encoded).to have_content('“twee retro man braid”')
       expect(mail.body.encoded).to have_content('listicle fam ramps flannel')
+      expect(mail.body.encoded).to have_content('https://doi.org/10.001/bc123df4567')
     end
   end
 
   describe 'approved_email' do
     let(:user) { work.depositor }
     let(:mail) { described_class.with(user: user, work_version: work_version).approved_email }
-    let(:work) { build_stubbed(:work, collection: collection, druid: 'druid:bc123df4567') }
+    let(:work) { build_stubbed(:work, collection: collection, druid: 'druid:bc123df4567', doi: '10.001/bc123df4567') }
     let(:work_version) { build_stubbed(:work_version, :deposited, title: 'Hammock kombucha mustache', work: work) }
     let(:collection) { build_stubbed(:collection, :with_reviewers, head: collection_version) }
     let(:collection_version) { build_stubbed(:collection_version, name: 'Farm-to-table beard aesthetic') }
@@ -82,6 +84,7 @@ RSpec.describe WorksMailer, type: :mailer do
     it 'renders the reason' do
       expect(mail.body.encoded).to have_content('“Hammock kombucha mustache”')
       expect(mail.body.encoded).to have_content('Farm-to-table beard aesthetic collection')
+      expect(mail.body.encoded).to have_content('https://doi.org/10.001/bc123df4567')
     end
   end
 
