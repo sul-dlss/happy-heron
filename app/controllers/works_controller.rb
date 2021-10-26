e6ce5a6628802060bcf0535f6e50ec5923f99916
Aaron Collier <aaron.collier@stanford.edu>
2021-10-26 14:25:50 -0700

    Include the orcid in contributor parameters

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -195,7 +195,7 @@ class WorksController < ObjectsController
                      subtype: [],
                      attached_files_attributes: %i[_destroy id label hide file],
                      authors_attributes: %i[_destroy id full_name first_name last_name role_term weight orcid],
-                     contributors_attributes: %i[_destroy id full_name first_name last_name role_term],
+                     contributors_attributes: %i[_destroy id full_name first_name last_name role_term weight orcid],
                      contact_emails_attributes: %i[_destroy id email],
                      keywords_attributes: %i[_destroy id label uri cocina_type],
                      related_works_attributes: %i[_destroy id citation],
