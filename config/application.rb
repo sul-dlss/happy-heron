8c775e1414f37338f51c2a7355ec7c3fdd415db3
Justin Coyne <jcoyne@justincoyne.com>
2022-08-03 08:22:34 -0500

    Switch to using Rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module HappyHeron
   # The applications configuration
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
