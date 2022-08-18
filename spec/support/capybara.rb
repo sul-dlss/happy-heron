cce62d0928bb1a7c04ce7f378020d3332051a75d
Justin Coyne <jcoyne@justincoyne.com>
2022-08-18 14:51:02 -0500

    Add a mechanism for displaying the javascript log when running Capybara tests

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,9 +4,39 @@ require 'capybara/cuprite'
 require 'capybara/rails'
 require 'capybara/rspec'
 
+class FerrumLogger
+  attr_reader :logs
+
+  def initialize
+    @logs = []
+  end
+
+  # Filter out the noise - I believe Runtime.exceptionThrown and Log.entryAdded are the interesting log methods but there might be others you need
+  def puts(log_str)
+    _log_symbol, _log_time, log_body_str = log_str.strip.split(' ', 3)
+    
+    return if log_body_str.nil?
+    
+    log_body = JSON.parse(log_body_str)
+
+    case log_body['method']
+    when "Runtime.consoleAPICalled"
+      log_body["params"]["args"].each do |arg|
+        Kernel.puts arg["value"]
+      end
+    when "Runtime.exceptionThrown", "Log.entryAdded"
+      Kernel.puts "#{log_body["params"]["entry"]["url"]} - #{log_body["params"]["entry"]["text"]}"
+    end
+  end
+
+  def truncate
+    @logs = []
+  end
+end
+
 Capybara.javascript_driver = :cuprite
 Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800], pending_connection_errors: false)
+  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800], pending_connection_errors: false, logger: FerrumLogger.new)
 end
 
 Capybara.disable_animation = true
