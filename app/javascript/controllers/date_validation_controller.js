f21beabc5d0bc1fa5d2f98e01dbfdaa83140723e
Justin Littman <justinlittman@stanford.edu>
2022-08-23 11:36:16 -0400

    Displays error message for invalid days.
    closes #2293

--- app/javascript/controllers/date_validation_controller.js
+++ app/javascript/controllers/date_validation_controller.js
@@ -38,31 +38,69 @@ export default class extends Controller {
     const month = this.toInt(this.monthTarget)
     const year = this.toInt(this.yearTarget)
 
+    if (!this.validateYearMin(year)) return
+    if (!this.validateYearPast(year, currentYear, month, day)) return
+    if (!this.validateMonthPast(year, currentYear, month, currentMonth, day)) return
+    if (!this.validateDayPast(year, currentYear, month, currentMonth, day, currentDay)) return
+    this.validateDayExists(month, day)
+  }
+
+  validateYearMin(year) {
     if (year != 0 && year < this.yearTarget.min) {
-      this.yearTarget.classList.add('is-invalid')
-      this.yearTarget.setCustomValidity('invalid')
-      this.errorTarget.textContent = 'must be after ' + this.yearTarget.min
-    } else if (year > currentYear) {
-      this.yearTarget.classList.add('is-invalid')
-      this.yearTarget.setCustomValidity('invalid')
-      this.errorTarget.textContent = 'must be in the past'
-    } else if (year == currentYear) {
-      if (month > currentMonth) {
-        this.monthTarget.classList.add('is-invalid')
-        this.monthTarget.setCustomValidity('invalid')
-        this.yearTarget.classList.add('is-invalid')
-        this.yearTarget.setCustomValidity('invalid')
-        this.errorTarget.textContent = 'must be in the past'
-      } else if (month == currentMonth && day > currentDay) {
-        this.dayTarget.classList.add('is-invalid')
-        this.dayTarget.setCustomValidity('invalid')
-        this.monthTarget.classList.add('is-invalid')
-        this.monthTarget.setCustomValidity('invalid')
-        this.yearTarget.classList.add('is-invalid')
-        this.yearTarget.setCustomValidity('invalid')
-        this.errorTarget.textContent = 'must be in the past'
-      }
+      this.invalid('must be after ' + this.yearTarget.min, false, false)
+      return false
     }
+    return true
+  }
+
+  validateYearPast(year, currentYear, month, day) {
+    if (year > currentYear) {
+      this.invalid('must be in the past', !!month, !!day)
+      return false
+    }
+    return true
+  }
+
+  validateMonthPast(year, currentYear, month, currentMonth, day) {
+    if (year == currentYear && month > currentMonth) {
+      this.invalid('must be in the past', true, !!day)
+      return false
+    }
+    return true
+  }
+
+  validateDayPast(year, currentYear, month, currentMonth, day, currentDay) {
+    if (year == currentYear && month == currentMonth && day > currentDay) {
+      this.invalid('must be in the past', true, true)
+      return false
+    }
+    return true
+  }
+
+  validateDayExists(month, day) {
+    if(!month || !day) return true
+
+    const lastDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if (lastDays[month - 1] < day) {
+      this.invalid('must be a valid day', true, true)
+      return false
+    }
+    return true
+  }
+
+  invalid(msg, invalid_month, invalid_day) {
+    this.yearTarget.classList.add('is-invalid')
+    this.yearTarget.setCustomValidity('invalid')  
+    if(invalid_month) {
+      this.monthTarget.classList.add('is-invalid')
+      this.monthTarget.setCustomValidity('invalid')
+    }
+    if(invalid_day) {
+      this.dayTarget.classList.add('is-invalid')
+      this.dayTarget.setCustomValidity('invalid')
+    }
+    this.errorTarget.textContent = msg
   }
 
   // Validate that the most signifcant parts are provided when a less significant part is provided.
