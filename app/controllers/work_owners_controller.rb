3f503a3997262754cf30981c205f2b781de76da5
Justin Coyne <jcoyne@justincoyne.com>
2022-08-24 12:01:04 -0500

    Add new owner as a depositor in a collection
    Fixes #2329

--- app/controllers/work_owners_controller.rb
+++ app/controllers/work_owners_controller.rb
@@ -13,16 +13,37 @@ class WorkOwnersController < ApplicationController
     authorize! :work_owner
 
     work = Work.find(params[:id])
-    orig_owner = work.owner
     new_owner = User.find_or_create_by(email: "#{params['sunetid']}@stanford.edu")
-    change_owner(work, new_owner)
-    create_event(work, new_owner, orig_owner)
+    move_work_to_new_owner(work, new_owner)
+    send_participant_change_emails(work.collection)
 
     flash[:success] = I18n.t('work.flash.owner_updated')
     redirect_to work_path(work)
   end
 
   private
+
+  def send_participant_change_emails(collection)
+    (collection.managed_by + collection.reviewed_by).uniq.each do |user|
+      CollectionsMailer.with(collection_version: collection.head, user: user)
+                       .participants_changed_email.deliver_later
+    end
+  end
+
+  def move_work_to_new_owner(work, new_owner)
+    orig_owner = work.owner
+
+    Work.transaction do
+      change_owner(work, new_owner)
+      create_event(work, new_owner, orig_owner)
+      add_depositor_to_collection(work.collection, new_owner)
+    end
+  end
+
+  def add_depositor_to_collection(collection, depositor)
+    collection.depositors << depositor
+    collection.save!
+  end
 
   def change_owner(work, owner)
     work.owner = owner
---
+++ spec/requests/change_work_owner_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Change owner of a work' do
+  let(:user) { create(:user) }
+  let(:orig_owner) { create(:user) }
+  let(:new_owner) { create(:user) }
+  let(:manager) { create(:user) }
+  let(:reviewer) { create(:user) }
+
+  let(:work_version) { create(:work_version, work: work) }
+  let(:collection_version) do
+    create(:collection_version_with_collection, managed_by: [manager], reviewed_by: [reviewer])
+  end
+  let(:work) { create(:work, owner: orig_owner, collection: collection_version.collection) }
+
+  before do
+    work.update(head: work_version)
+    sign_in user, groups: ['dlss:hydrus-app-administrators']
+  end
+
+  it 'allows owner to be changed' do
+    expect { put owners_path(work), params: { sunetid: new_owner.sunetid } }
+      .to have_enqueued_job(ActionMailer::MailDeliveryJob).exactly(:twice)
+
+    follow_redirect!
+    # Flash message
+    expect(response.body).to include 'Owner updated'
+    expect(work.collection.reload.depositors).to include new_owner
+  end
+end
