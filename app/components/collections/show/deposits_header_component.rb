e36b97fb6881475c1ebed952bee739ec24c98370
Justin Littman <justinlittman@stanford.edu>
2022-08-30 11:38:15 -0400

    Update links for collection editing.
    closes #2288

---
+++ app/components/collections/show/deposits_header_component.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Collections
+  module Show
+    # Renders the header for the collection deposits page.
+    # This component ought to have a sibling component of Works::WorkTypeModalComponent
+    # and it ought to be within a data-controller="work-type" attribute because
+    # this component contains a button that acts on that controller
+    class DepositsHeaderComponent < SettingsHeaderComponent
+      def edit_button
+        return unless draft?
+
+        link_to 'Edit or Deposit', edit_collection_version_path(collection_version),
+                class: 'btn btn-outline-primary me-2'
+      end
+    end
+  end
+end
---
+++ spec/components/collections/show/deposits_header_component_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Collections::Show::DepositsHeaderComponent, type: :component do
+  let(:rendered) { render_inline(described_class.new(collection_version: collection_version)) }
+
+  context 'with a new, first draft collection' do
+    let(:collection_version) { build_stubbed(:collection_version, :first_draft) }
+    let(:collection_id) { collection_version.collection_id }
+
+    it 'does not render the spinner and renders the edit link' do
+      expect(rendered.to_html).not_to include 'fa-spinner'
+      expect(rendered.css('a.btn').text).to eq 'Edit or Deposit'
+    end
+
+    it 'renders turbo frames' do
+      expect(rendered.css('turbo-frame')[0]['src']).to eq "/collections/#{collection_id}/deposit_button"
+      expect(rendered.css('turbo-frame')[1]['src']).to eq "/collection_versions/#{collection_version.id}/edit_link"
+    end
+  end
+
+  context 'with a depositing collection' do
+    let(:collection_version) { build_stubbed(:collection_version, :depositing) }
+
+    it 'renders the spinner' do
+      expect(rendered.to_html).to include 'Depositing'
+      expect(rendered.to_html).to include 'fa-solid fa-spinner fa-pulse'
+      expect(rendered.css('a.btn')).to be_empty
+    end
+  end
+
+  context 'when the collection is deposited' do
+    let(:collection_version) { build_stubbed(:collection_version, :deposited) }
+
+    it 'does not render the link' do
+      expect(rendered.css('a.btn')).to be_empty
+    end
+  end
+end
