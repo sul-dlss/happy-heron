24602a2e8318900c93487d8f55af29757f0d9da3
Justin Littman <justinlittman@stanford.edu>
2022-08-30 13:44:50 -0400

    Allow changing owner to an existing depositor.
    closes #2496

--- spec/requests/change_work_owner_spec.rb
+++ spec/requests/change_work_owner_spec.rb
@@ -32,6 +32,25 @@ RSpec.describe 'Change owner of a work' do
     end
   end
 
+  context 'when new owner is already a depositor' do
+    let(:collection) { collection_version.collection }
+
+    before do
+      collection.depositors << new_owner
+      collection.save!
+    end
+
+    it 'allows owner to be changed' do
+      expect(collection.depositors).to include new_owner
+      put owners_path(work), params: { sunetid: new_owner.sunetid }
+
+      follow_redirect!
+      # Flash message
+      expect(response.body).to include 'Owner updated'
+      expect(collection.reload.depositors).to include new_owner
+    end
+  end
+
   context 'when new owner is same as original owner' do
     it 'prevents owner from being changed' do
       expect { put owners_path(work), params: { sunetid: orig_owner.sunetid } }
