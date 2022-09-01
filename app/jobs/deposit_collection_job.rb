39038b122c7894070c99ff3d5035bfeeea8a1d6c
Justin Littman <justinlittman@stanford.edu>
2022-09-01 14:52:52 -0400

    Submit version description for collections.
    closes #2563

--- app/jobs/deposit_collection_job.rb
+++ app/jobs/deposit_collection_job.rb
@@ -5,22 +5,23 @@ class DepositCollectionJob < BaseDepositJob
   queue_as :default
 
   def perform(collection_version)
-    deposit(request_dro: CocinaGenerator::CollectionGenerator.generate_model(collection_version: collection_version))
+    deposit(request_dro: CocinaGenerator::CollectionGenerator.generate_model(collection_version: collection_version),
+            version_description: collection_version.version_description.presence)
   rescue StandardError => e
     Honeybadger.notify(e)
   end
 
   private
 
-  def deposit(request_dro:)
+  def deposit(request_dro:, version_description:)
     login_result = login
 
     raise login_result.failure unless login_result.success?
 
-    create_or_update(request_dro)
+    create_or_update(request_dro, version_description)
   end
 
-  def create_or_update(request_dro)
+  def create_or_update(request_dro, version_description)
     case request_dro
     when Cocina::Models::RequestCollection
       SdrClient::Deposit::CreateResource.run(accession: true,
@@ -30,7 +31,8 @@ class DepositCollectionJob < BaseDepositJob
     when Cocina::Models::Collection
       SdrClient::Deposit::UpdateResource.run(metadata: request_dro,
                                              logger: Rails.logger,
-                                             connection: connection)
+                                             connection: connection,
+                                             version_description: version_description)
     end
   end
 
--- spec/jobs/deposit_collection_job_spec.rb
+++ spec/jobs/deposit_collection_job_spec.rb
@@ -25,6 +25,24 @@ RSpec.describe DepositCollectionJob do
     end
   end
 
+  context 'when the deposit update request is successful' do
+    let(:collection) { build_stubbed(:collection, :with_collection_druid) }
+    let(:collection_version) { build_stubbed(:collection_version, :with_version_description, collection: collection) }
+
+    before do
+      allow(SdrClient::Deposit::UpdateResource).to receive(:run).and_return(1234)
+    end
+
+    it 'initiates an update' do
+      described_class.perform_now(collection_version)
+      expect(SdrClient::Deposit::UpdateResource).to have_received(:run)
+        .with(metadata: instance_of(Cocina::Models::Collection),
+              logger: Rails.logger,
+              connection: conn,
+              version_description: collection_version.version_description)
+    end
+  end
+
   context 'when the deposit request is not successful' do
     before do
       allow(SdrClient::Deposit::CreateResource).to receive(:run).and_raise('Deposit failed.')
