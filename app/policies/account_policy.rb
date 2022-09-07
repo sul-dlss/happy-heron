bfeb99c540bfdeda2227a60417efe3e962d7b626
Justin Coyne <jcoyne@justincoyne.com>
2022-09-07 14:44:08 -0500

    Expand account policy to collection managers
    This allows collection managers to lookup accounts
    
    Fixes #2510

--- app/policies/account_policy.rb
+++ app/policies/account_policy.rb
@@ -3,8 +3,14 @@
 # Authorization policy for lookup of accounts
 class AccountPolicy < ApplicationPolicy
   def show?
-    administrator? || collection_creator?
+    administrator? || collection_creator? || manages_any_collection?
   end
 
   delegate :administrator?, :collection_creator?, to: :user_with_groups
+
+  private
+
+  def manages_any_collection?
+    user_with_groups.user.manages_collections.any?
+  end
 end
