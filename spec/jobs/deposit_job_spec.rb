e49911f6fa1bf4eb0794f487646fe1bbd68c12ba
Justin Littman <justinlittman@stanford.edu>
2022-09-09 14:37:58 -0400

    Only uploads changed files.
    closes #2638

--- spec/jobs/deposit_job_spec.rb
+++ spec/jobs/deposit_job_spec.rb
@@ -75,66 +75,66 @@ RSpec.describe DepositJob do
     let(:work) { build(:work, collection: collection, assign_doi: false, druid: druid) }
     let(:druid) { 'druid:bf024yb8975' }
 
-    before do
-      allow(SdrClient::Deposit::UpdateResource).to receive(:run).and_return(1234)
+    # The job fetches the existing cocina model from the SDR API to copy structural > contains.
+    let(:cocina) do
+      {
+        cocinaVersion: Cocina::Models::VERSION,
+        externalIdentifier: druid,
+        type: Cocina::Models::ObjectType.book,
+        label: 'Test DRO',
+        version: 1,
+        description: {
+          title: [{ value: 'Test DRO' }],
+          purl: "https://purl.stanford.edu/#{druid.delete_prefix('druid:')}"
+        },
+        access: { view: 'world', download: 'world' },
+        administrative: { hasAdminPolicy: 'druid:hy787xj5878' },
+        identification: { sourceId: 'sul:abc123' },
+        structural: { contains: [
+          {
+            type: Cocina::Models::FileSetType.file,
+            externalIdentifier: 'https://cocina.sul.stanford.edu/fileSet/123-456-789', label: 'Page 1', version: 1,
+            structural: {
+              contains: [
+                {
+                  type: Cocina::Models::ObjectType.file,
+                  externalIdentifier: 'https://cocina.sul.stanford.edu/file/123-456-789',
+                  label: '00001.html',
+                  filename: '00001.html',
+                  size: 0,
+                  version: 1,
+                  hasMimeType: 'text/html',
+                  use: 'transcription',
+                  hasMessageDigests: [
+                    {
+                      type: 'sha1', digest: 'cb19c405f8242d1f9a0a6180122dfb69e1d6e4c7'
+                    }, {
+                      type: 'md5', digest: 'f5eff9e28f154f79f7a11261bc0d4b30'
+                    }
+                  ],
+                  access: { view: 'dark' },
+                  administrative: { publish: false, sdrPreserve: true, shelve: false }
+                }
+              ]
+            }
+          }
+        ] }
+      }
     end
 
-    context 'when files have not changed and SDR find succeeds' do
+    before do
+      allow(SdrClient::Deposit::UpdateResource).to receive(:run).and_return(1234)
+      allow(SdrClient::Find).to receive(:run).and_return(cocina.to_json)
+    end
+
+    context 'when files have not changed' do
       # The attached files for this version are the same as the previous version.
       let(:second_work_version_metadata_only) do
         build(:work_version, work: work, attached_files: [attached_file], version: 2,
                              version_description: 'Updated metadata')
       end
 
-      # The job fetches the existing cocina model from the SDR API to copy structural > contains.
-      let(:cocina) do
-        {
-          cocinaVersion: Cocina::Models::VERSION,
-          externalIdentifier: druid,
-          type: Cocina::Models::ObjectType.book,
-          label: 'Test DRO',
-          version: 1,
-          description: {
-            title: [{ value: 'Test DRO' }],
-            purl: "https://purl.stanford.edu/#{druid.delete_prefix('druid:')}"
-          },
-          access: { view: 'world', download: 'world' },
-          administrative: { hasAdminPolicy: 'druid:hy787xj5878' },
-          identification: { sourceId: 'sul:abc123' },
-          structural: { contains: [
-            {
-              type: Cocina::Models::FileSetType.file,
-              externalIdentifier: 'https://cocina.sul.stanford.edu/fileSet/123-456-789', label: 'Page 1', version: 1,
-              structural: {
-                contains: [
-                  {
-                    type: Cocina::Models::ObjectType.file,
-                    externalIdentifier: 'https://cocina.sul.stanford.edu/file/123-456-789',
-                    label: '00001.html',
-                    filename: '00001.html',
-                    size: 0,
-                    version: 1,
-                    hasMimeType: 'text/html',
-                    use: 'transcription',
-                    hasMessageDigests: [
-                      {
-                        type: 'sha1', digest: 'cb19c405f8242d1f9a0a6180122dfb69e1d6e4c7'
-                      }, {
-                        type: 'md5', digest: 'e6d52da47a5ade91ae31227b978fb023'
-                      }
-                    ],
-                    access: { view: 'dark' },
-                    administrative: { publish: false, sdrPreserve: true, shelve: false }
-                  }
-                ]
-              }
-            }
-          ] }
-        }
-      end
-
       before do
-        allow(SdrClient::Find).to receive(:run).and_return(cocina.to_json)
         work.work_versions = [first_work_version, second_work_version_metadata_only]
       end
 
@@ -144,8 +144,10 @@ RSpec.describe DepositJob do
         # Notice that UpdateResource.run is called but UploadFiles.upload is not.
         # This makes this a "metadata only" update.
         expect(SdrClient::Deposit::UpdateResource).to have_received(:run)
-          .with(a_hash_including(version_description: 'Updated metadata'))
-        expect(SdrClient::Find).to have_received(:run)
+          .with(a_hash_including(version_description: 'Updated metadata')) do |params|
+          external_identifier = params[:metadata].structural.contains.first.structural.contains.first.externalIdentifier
+          expect(external_identifier).to eq('https://cocina.sul.stanford.edu/file/123-456-789')
+        end
       end
     end
 
@@ -159,7 +161,11 @@ RSpec.describe DepositJob do
       it 'uploads files and calls UpdateResource.run' do
         described_class.perform_now(second_work_version)
         expect(SdrClient::Deposit::UpdateResource).to have_received(:run)
-          .with(a_hash_including(version_description: 'Changed files'))
+          .with(a_hash_including(version_description: 'Changed files')) do |params|
+          external_identifier = params[:metadata].structural.contains.first.structural.contains.first.externalIdentifier
+          expect(external_identifier).to eq('9999999')
+        end
+
         expect(SdrClient::Deposit::UploadFiles).to have_received(:upload)
       end
     end
