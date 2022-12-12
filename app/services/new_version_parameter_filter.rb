6c16c4015f7eb4ca430fde54a510a2071d5dd5d8
Justin Littman <justinlittman@stanford.edu>
2022-12-12 11:20:38 -0500

    Allow NewVersionParameterFilter to handle non-existing existing params.
    closes #2891

--- app/services/new_version_parameter_filter.rb
+++ app/services/new_version_parameter_filter.rb
@@ -17,6 +17,8 @@ class NewVersionParameterFilter
   def self.filter_attached_files(clean_params, old_version)
     old_version.attached_files.each do |existing|
       existing_params = clean_params[:attached_files_attributes].values.find { |hash| hash['id'] == existing.id.to_s }
+      next if existing_params.blank?
+
       existing_params['file'] = existing.file_attachment.blob.signed_id
       existing_params.delete('id')
     end
@@ -26,6 +28,8 @@ class NewVersionParameterFilter
   def self.filter_relation_params(clean_params, old_version, relation)
     old_version.public_send(relation).each do |existing|
       existing_params = clean_params["#{relation}_attributes"].values.find { |hash| hash['id'] == existing.id.to_s }
+      next if existing_params.blank?
+
       existing_params.delete('id')
     end
   end
