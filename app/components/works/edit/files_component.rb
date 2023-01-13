0f4a3504c457f8cf534ad2592dee64680f92ef51
Peter Mangiafico <peter@mangiafico.org>
2023-01-13 13:27:59 -0800

    edit label in hierarchy

--- app/components/works/edit/files_component.rb
+++ app/components/works/edit/files_component.rb
@@ -4,11 +4,12 @@ module Works
   module Edit
     # Displays the list of files available to edit in the work edit page
     class FilesComponent < ViewComponent::Base
-      def initialize(work_version:)
+      def initialize(work_version:, form:)
         @work_version = work_version
+        @form = form
       end
 
-      attr_reader :work_version
+      attr_reader :work_version, :form
 
       def root_directory
         FileHierarchyService.to_hierarchy(work_version:)
