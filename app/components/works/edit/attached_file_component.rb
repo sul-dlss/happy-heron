806427d53dae468d8223a607c52543eac77e8e58
Peter Mangiafico <peter@mangiafico.org>
2023-01-13 16:39:42 -0800

    remove unused code

--- app/components/works/edit/attached_file_component.rb
+++ app/components/works/edit/attached_file_component.rb
@@ -12,24 +12,11 @@ module Works
 
       attr_reader :attached_file, :depth, :form
 
-      delegate :basename, :label, :hide?, :in_preservation?, :in_globus?, :file, to: :attached_file
-
-      # @return a link to download the file. If the file is new in this version, then it generates an
-      # activeStorage link, otherwise a preservation link.
-      def path
-        return preservation_path(attached_file) if in_preservation?
-
-        rails_blob_path(file, disposition: 'attachment')
-      end
+      delegate :basename, to: :attached_file
 
       # file has been uploaded but may not yet have been saved to the database model (and may not validate)
       def uploaded?
         !attached_file.blob.nil?
-      end
-
-      # the user can get a download link unless the file is in globus (in which case, no download link is available)
-      def can_download?
-        !in_globus?
       end
     end
   end
