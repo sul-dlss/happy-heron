2b88730380b644f9ad8269ebebf6e9574e303412
Aaron Collier <aaron.collier@stanford.edu>
2023-07-11 09:00:13 -0700

    Improve performance of collection deposits page

--- app/components/collections/work_row_component.rb
+++ app/components/collections/work_row_component.rb
@@ -13,7 +13,14 @@ module Collections
 
     # Returns the size of the attached files
     def size
-      number_to_human_size(attached_files.sum(&:byte_size))
+      # Note: This direct SQL query avoids excessive queries for large objects,
+      # Otherwise multiple queries to ActiveRecord are performed for each file
+      # to get it's size.
+      number_to_human_size(AttachedFile.where(work_version_id: work_version.id) \
+                           .joins("INNER JOIN active_storage_attachments on active_storage_attachments.record_id = attached_files.id")
+                           .joins("INNER JOIN active_storage_blobs on active_storage_attachments.blob_id = active_storage_blobs.id")
+                           .where("active_storage_attachments.record_type" => "AttachedFile")
+                           .sum("byte_size"))
     end
 
     delegate :work, :attached_files, to: :work_version
