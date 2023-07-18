ec664051ec3d1022d79b0a0ce0a70b4700e4caa7
Justin Littman <justinlittman@stanford.edu>
2023-07-18 14:15:26 -0400

    Adds rake task for getting Stanford users not in MAIS.
    closes #3154

---
+++ app/services/orcid_stanford_user_service.rb
@@ -0,0 +1,24 @@
+# Finds contributors that have an orcid id and a Stanford affiliation, but do not have an orcid in MAIS.
+class OrcidStanfordUserService
+  def self.execute
+    new.execute
+  end
+
+  def execute
+    orcid_ids = Affiliation.where("label ILIKE ?", "%Stanford University%").joins(:abstract_contributor).where.not(abstract_contributor: {orcid: nil}).pluck(:orcid).uniq
+    contributors = orcid_ids.map { |orcid_id| AbstractContributor.find_by(orcid: orcid_id) }
+    contributors.select do |contributor|
+      mais_orcid_client.fetch_orcid_user(orcidid: contributor.orcid).nil?
+    end
+  end
+
+  private
+
+  def mais_orcid_client
+    @mais_orcid_client ||= MaisOrcidClient.configure(
+      client_id: Settings.mais_orcid.client_id,
+      client_secret: Settings.mais_orcid.client_secret,
+      base_url: Settings.mais_orcid.base_url
+    )
+  end
+end
