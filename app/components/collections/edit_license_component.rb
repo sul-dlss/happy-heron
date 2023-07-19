9976cf2388df9972a8e0957f62caccb0ceceac45
Justin Littman <justinlittman@stanford.edu>
2023-07-19 08:12:35 -0400

    Fix to edit license component.

--- app/components/collections/edit_license_component.rb
+++ app/components/collections/edit_license_component.rb
@@ -12,24 +12,24 @@ module Collections
     delegate :default_license, :required_license, to: :collection_form
 
     def collection_form
+      # Can be a DraftCollectionForm or a CollectionSettingsForm
       form.object
     end
 
     def collection
-      case form.object.model
-      when Collection
-        form.object.model
-      when CollectionVersion
-        form.object.model.collection
-      end
+      # If a DraftCollectionForm, the model is a Hash
+      return form.object.model.fetch(:collection) if form.object.model.is_a?(Hash)
+
+      # If a CollectionSettingsForm, the model is a Collection
+      form.object.model
     end
 
     def custom_rights_statement_source_option
-      collection&.custom_rights_statement_source_option
+      collection.custom_rights_statement_source_option
     end
 
     def custom_rights_instructions_source_option
-      collection&.custom_rights_instructions_source_option
+      collection.custom_rights_instructions_source_option
     end
 
     def error?
