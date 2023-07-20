833d5d619cbc8670da56e71edf6e777d693769a6
Peter Mangiafico <peter@mangiafico.org>
2023-07-20 13:32:05 -0700

    do not render affiliation component

--- app/components/works/affiliations_component.rb
+++ app/components/works/affiliations_component.rb
@@ -8,5 +8,12 @@ module Works
     end
 
     attr_reader :form
+
+    # NOTE: July 20 2023 Affiliations are not working correctly due to issues with nested forms and reform.
+    # Do not render the control until we correct this.
+    # see https://github.com/sul-dlss/happy-heron/issues/3285
+    def render?
+      false
+    end
   end
 end
