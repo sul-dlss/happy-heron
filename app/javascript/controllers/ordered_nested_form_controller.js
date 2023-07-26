4e30b0e4ac0d4c9ce11a53f9a26863ba912c4dac
Peter Mangiafico <peter@mangiafico.org>
2023-07-26 12:04:14 -0700

    ensure move/up down button targets only contributor row elements

--- app/javascript/controllers/ordered_nested_form_controller.js
+++ app/javascript/controllers/ordered_nested_form_controller.js
@@ -33,7 +33,7 @@ export default class extends NestedFormController {
 
   moveUp(event) {
     const item = this.getItemForButton(event.target)
-    const previous = item.previousElementSibling
+    const previous = this.getPreviousSibling(item, 'div.contributor-row')
     previous.remove()
     item.insertAdjacentElement('afterend', previous)
     this.renumber()
@@ -41,7 +41,7 @@ export default class extends NestedFormController {
 
   moveDown(event) {
     const item = this.getItemForButton(event.target)
-    const next = item.nextElementSibling
+    const next = this.getNextSibling(item, 'div.contributor-row')
     next.remove()
     item.insertAdjacentElement('beforebegin', next)
     this.renumber()
@@ -61,4 +61,39 @@ export default class extends NestedFormController {
     const allRows = this.element.querySelectorAll(this.selectorValue)
     return Array.from(allRows).filter((item) => item.querySelector("input[name*='_destroy']").value != "1")
   }
+
+  // These methods help us find the next or previous element that also match a given selector.
+  // This allows us to better target a specific element of interest.
+  getNextSibling(elem, selector) {
+    // Get the next sibling element
+    // this is native JS: https://developer.mozilla.org/en-US/docs/Web/API/Element/nextElementSibling
+    var sibling = elem.nextElementSibling
+
+    // If there's no selector, return the sibling
+    if (!selector) return sibling
+
+    // If the sibling matches our selector, use it
+    // If not, jump to the next sibling and continue the loop
+    while (sibling) {
+      if (sibling.matches(selector)) return sibling
+      sibling = sibling.nextElementSibling
+    }
+  }
+
+  getPreviousSibling(elem, selector) {
+    // Get the previous sibling element
+    // this is native JS: https://developer.mozilla.org/en-US/docs/Web/API/Element/previousElementSibling
+    var sibling = elem.previousElementSibling
+
+    // If there's no selector, return the sibling
+    if (!selector) return sibling
+
+    // If the sibling matches our selector, use it
+    // If not, jump to the previous sibling and continue the loop
+    while (sibling) {
+      if (sibling.matches(selector)) return sibling
+      sibling = sibling.previousElementSibling
+    }
+  }
+
 }
