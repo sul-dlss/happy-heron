25b25953ec6a526af23562ff8f9587056f4581bf
Johnathan Martin <john.martin@stanford.edu>
2023-07-25 23:50:04 -0700

    minor cruft removal for edit_license_component and edit_terms_of_use_component

--- app/components/collections/edit_license_component.rb
+++ app/components/collections/edit_license_component.rb
@@ -16,22 +16,6 @@ module Collections
       form.object
     end
 
-    def collection
-      # If a DraftCollectionForm, the model is a Hash
-      return form.object.model.fetch(:collection) if form.object.model.is_a?(Hash)
-
-      # If a CollectionSettingsForm, the model is a Collection
-      form.object.model
-    end
-
-    def custom_rights_statement_source_option
-      collection.custom_rights_statement_source_option
-    end
-
-    def custom_rights_instructions_source_option
-      collection.custom_rights_instructions_source_option
-    end
-
     def error?
       errors.present?
     end
