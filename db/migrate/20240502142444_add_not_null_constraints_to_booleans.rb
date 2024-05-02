2b19ff23407854a55f7d0207f258bc5f84094354
Laura Wrubel <lwrubel@stanford.edu>
2024-05-02 16:26:15 -0400

    change_columns_default then null

--- db/migrate/20240502142444_add_not_null_constraints_to_booleans.rb
+++ db/migrate/20240502142444_add_not_null_constraints_to_booleans.rb
@@ -1,13 +1,13 @@
 class AddNotNullConstraintsToBooleans < ActiveRecord::Migration[7.0]
     def change
+      change_column_default :collections, :email_when_participants_changed, true
+      change_column_default :collections, :email_depositors_status_changed, true
+      
       change_column_null :collections, :email_when_participants_changed, false
       change_column_null :collections, :email_depositors_status_changed, false
       change_column_null :collections, :review_enabled, false
   
       change_column_null :page_contents, :visible, false
       change_column_null :page_contents, :link_visible, false
-  
-      change_column_default :collections, :email_when_participants_changed, true
-      change_column_default :collections, :email_depositors_status_changed, true
     end
   end
