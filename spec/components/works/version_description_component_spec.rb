79a9ea032dbe1289cdf995a7d89403f4fe61e44f
Laura Wrubel <lwrubel@stanford.edu>
2024-09-12 16:54:12 -0400

    Version component displays in review worfklow

--- spec/components/works/version_description_component_spec.rb
+++ spec/components/works/version_description_component_spec.rb
@@ -51,5 +51,37 @@ RSpec.describe Works::VersionDescriptionComponent, type: :component do
       expect(rendered.to_html).to include('Do you want to create a new version of this deposit?')
       expect(rendered.to_html).not_to include('Version your work')
     end
+
+    context 'with a pending_approval work' do
+      let(:work_version) { build(:work_version, work:, state: 'pending_approval') }
+
+      it 'does not render the component' do
+        expect(rendered.to_html).not_to include('Do you want to create a new version of this deposit?')
+      end
+    end
+
+    context 'with a pending_approval work that has been accessioned' do
+      let(:work_version) { build(:work_version, work:, version: 2, state: 'pending_approval') }
+
+      it 'renders the component' do
+        expect(rendered.to_html).to include('Do you want to create a new version of this deposit?')
+      end
+    end
+
+    context 'with a rejected work' do
+      let(:work_version) { build(:work_version, work:, state: 'rejected') }
+
+      it 'does not render the component' do
+        expect(rendered.to_html).not_to include('Do you want to create a new version of this deposit?')
+      end
+    end
+
+    context 'with user versions and a rejected work that has been accessioned' do
+      let(:work_version) { build(:work_version, work:, version: 2, state: 'rejected') }
+
+      it 'renders the component' do
+        expect(rendered.to_html).to include('Do you want to create a new version of this deposit?')
+      end
+    end
   end
 end
