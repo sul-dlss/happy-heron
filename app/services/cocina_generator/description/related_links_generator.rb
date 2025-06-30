789d1eba52e643f29a413ab1bfafe647a1694d88
Laura Wrubel <lwrubel@stanford.edu>
2025-06-30 17:18:59 -0400

    Map related links to related resources

--- app/services/cocina_generator/description/related_links_generator.rb
+++ app/services/cocina_generator/description/related_links_generator.rb
@@ -14,7 +14,12 @@ module CocinaGenerator
       end
 
       def generate
-        object.related_links.map { |rel_link| build_related_link(rel_link) }
+        if Settings.map_related_links_to_resources
+          # map all links into related resources for compatibility with H3
+          object.related_links.map { |rel_link| build_related_resources(rel_link) }
+        else
+          object.related_links.map { |rel_link| build_related_link(rel_link) }
+        end
       end
 
       private
@@ -33,17 +38,47 @@ module CocinaGenerator
         Cocina::Models::RelatedResource.new(resource_attrs)
       end
 
+      # For H3 compatibility where related links are mapped to related resources
+      def build_related_resources(rel_link)
+        return purl_link(rel_link) if purl?(rel_link.url)
+
+        resource_attrs = if uri_type_for(rel_link.url).present?
+                           {
+                             identifier: [Cocina::Models::DescriptiveValue.new(uri: rel_link.url,
+                                                                               type: uri_type_for(rel_link.url))]
+                           }
+                         else
+                           {
+                             access: Cocina::Models::DescriptiveAccessMetadata.new(
+                               url: [Cocina::Models::DescriptiveValue.new(value: rel_link.url)]
+                             )
+                           }
+                         end
+
+        Cocina::Models::RelatedResource.new(resource_attrs)
+      end
+
       # This normalizes the PURL link to https (as it is currently the canonincal PURL)
       def purl_link(rel_link)
         resource_attrs = {
           purl: rel_link.url.sub(/^https?/, 'https')
         }
-        resource_attrs[:title] = [{ value: rel_link.link_title }] if rel_link.link_title.present?
+        # only include title attributes for H2 mappings
+        if rel_link.link_title.present? && !Settings.map_related_links_to_resources
+          resource_attrs[:title] = [{ value: rel_link.link_title }]
+        end
         Cocina::Models::RelatedResource.new(resource_attrs)
       end
 
       def purl?(url)
         url.start_with?("https://#{purl_host}") || url.start_with?("http://#{purl_host}")
+      end
+
+      def uri_type_for(rel_link)
+        return 'doi' if rel_link.include?('doi.org')
+        return 'arxiv' if rel_link.include?('arxiv.org')
+
+        'pmid' if rel_link.include?('pubmed.ncbi.nlm.nih.gov')
       end
     end
   end
--- spec/services/cocina_generator/description/related_links_generator_spec.rb
+++ spec/services/cocina_generator/description/related_links_generator_spec.rb
@@ -60,4 +60,90 @@ RSpec.describe CocinaGenerator::Description::RelatedLinksGenerator do
                           ])
     end
   end
+
+  context 'with map_related_resources feature flag' do
+    before do
+      allow(Settings).to receive(:map_related_links_to_resources).and_return(true)
+    end
+
+    context 'with external links' do
+      let(:work_version) do
+        build(:work_version, related_links: [build(:related_link), build(:related_link)])
+      end
+
+      it 'creates related resources without titles' do
+        expect(model).to eq([
+                              Cocina::Models::RelatedResource.new({
+                                                                    access: { url: [
+                                                                      { value: 'http://my.awesome.research.io' }
+                                                                    ] }
+                                                                  }).to_h,
+                              Cocina::Models::RelatedResource.new({
+                                                                    access: { url: [
+                                                                      { value: 'http://my.awesome.research.io' }
+                                                                    ] }
+                                                                  }).to_h
+                            ])
+      end
+    end
+
+    context 'with PURL URIs' do
+      let(:work_version) do
+        build(:work_version, related_links: [
+                build(:related_link, url: 'http://purl.stanford.edu/tx853fp2857'),
+                build(:related_link, url: 'https://purl.stanford.edu/xy933bc2222'),
+                build(:related_link)
+              ])
+      end
+
+      it 'creates related links' do
+        expect(model).to eq([
+                              Cocina::Models::RelatedResource.new({
+                                                                    purl: 'https://purl.stanford.edu/tx853fp2857'
+                                                                  }).to_h,
+                              Cocina::Models::RelatedResource.new({
+                                                                    purl: 'https://purl.stanford.edu/xy933bc2222'
+                                                                  }).to_h,
+                              Cocina::Models::RelatedResource.new({
+                                                                    access: { url: [
+                                                                      { value: 'http://my.awesome.research.io' }
+                                                                    ] }
+                                                                  }).to_h
+                            ])
+      end
+    end
+
+    context 'with other URIs' do
+      let(:work_version) do
+        build(:work_version, related_links: [
+                build(:related_link, url: 'https://doi.org/10.1126/science.aar3646'),
+                build(:related_link, url: 'https://arxiv.org/abs/1706.03762'),
+                build(:related_link, url: 'https://pubmed.ncbi.nlm.nih.gov/31060017/')
+              ])
+      end
+
+      it 'creates related resources' do
+        expect(model).to eq([
+                              Cocina::Models::RelatedResource.new({
+                                                                    identifier: [{
+                                                                      uri: 'https://doi.org/10.1126/science.aar3646',
+                                                                      type: 'doi'
+                                                                    }]
+                                                                  }).to_h,
+                              Cocina::Models::RelatedResource.new({
+                                                                    identifier: [{
+                                                                      uri: 'https://arxiv.org/abs/1706.03762',
+                                                                      type: 'arxiv'
+                                                                    }]
+                                                                  }).to_h,
+                              Cocina::Models::RelatedResource.new({
+                                                                    identifier: [{
+                                                                      uri: 'https://pubmed.ncbi.nlm.nih.gov/31060017/',
+                                                                      type: 'pmid'
+                                                                    }]
+                                                                  }).to_h
+                            ])
+      end
+    end
+  end
 end
